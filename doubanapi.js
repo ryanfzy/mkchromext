32806ddadd94cf633e027268e6c0bdb6c680f604
Ziyi <ryanfzyw@gmail.com>
2015-03-11 21:47:48 +1300

    add apis.add_api() method; but not finished, keeping doing it

--- doubanapi.js
+++ doubanapi.js
@@ -1,9 +1,12 @@
 angular.module('doubanModule', []).factory('doubanapi', ['$http',
 	function($http){
-		var dbapi = 'https://api.douban.com/v2/movie/search?q=';
-		var searchapi = function(search_for, onsuccess){
+		var count = 0;
+		var dburl = 'https://api.douban.com';
+		var searchapi = dburl + '/v2/movie/search?q=';
+		var subjectapi = dburl + '/v2/movie/subject/';
+		var searchSubjects = function(search_for, onsuccess){
 			var res = [];
-			var url = dbapi + encodeURIComponent(search_for);
+			var url = searchapi + encodeURIComponent(search_for);
 			$http.get(url).success(function(data){
 				var sbjs = data.subjects;
 				for(var i = 0; i < sbjs.length; i++){
@@ -23,13 +26,29 @@ angular.module('doubanModule', []).factory('doubanapi', ['$http',
 						result.actors += (as[j].name + ' ');
 					}
 					result.alt = sbjs[i].alt;
+					result.id = sbjs[i].id;
 					res.push(result);
 				}
 				onsuccess(res);
 			});
 		};
+		var getSubject = function(id, onsuccess){
+			var url = subjectapi + id;
+			onsuccess(url);
+			/*
+			$http.get(url).success(function(data){
+
+			});
+			*/
+		};
+		var test = function(){
+			count++;
+			alert(count);
+		};
 		return {
-			search: searchapi
+			search: searchSubjects,
+			getOne: getSubject,
+			test: test
 		}
 	}
 ]);
