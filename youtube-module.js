5d05cae59ac148658ec1d7f048077b49645062c9
Ziyi <ryanfzyw@gmail.com>
2015-05-07 23:09:35 +1200

    can fetch the movie trailer yoututbe, need to make use of the url to the video

--- youtube-module.js
+++ youtube-module.js
@@ -1,14 +1,19 @@
+/* from the api key from used by other people */
+/*
 chrome.webRequest.onBeforeSendHeaders.addListener(function(details){
     details.requestHeaders.push({name: 'Referer', value: 'moviekong.com'});
     return {requestHeaders : details.requestHeaders};
 }, {urls: ['<all_urls>']}, ['requestHeaders']);
+*/
 
 angular.module('youtubeModule', []).factory('youtubeapi', ['$http',
 	function($http){
         var key = 'AIzaSyBdJBVaF4Hy-Zlc_y3F5DU8H1iK1BRCHA4';
-        var url = 'https://www.googleapis.com/youtube/v3/search';
-        var api = createAPI.createService(url);
-        api.addRequestTags(['channelId', 'channelType', 'eventType', 'location',
+        var searchurl = 'https://www.googleapis.com/youtube/v3/search';
+        var videourl = 'https://www.googleapis.com/youtube/v3/videos';
+        var watchurl = 'https://www.youtube.com/watch?{v}';
+        var searchapi = createAPI.createService(searchurl);
+        searchapi.addRequestTags(['channelId', 'channelType', 'eventType', 'location',
             'locationRadius', 'maxResults', 'order', 'pageToken', 'publishedAfter',
             'publishedBefore', 'q', 'regionCode', 'relevanceLanguage', 'safeSearch',
             'topicId', 'type', 'videoCaption', 'videoCategoryId', 'videoDefinition',
@@ -16,19 +21,26 @@ angular.module('youtubeModule', []).factory('youtubeapi', ['$http',
             'videoSyndicated', 'videoType', 'key', 'part']
         );
 
+        var videoapi = createAPI.createService(videourl);
+        videoapi.addRequestTags(['part', 'id', 'key']);
+
+        var watchapi = createAPI.createService(watchurl);
+
         //$http.defaults.headers.common.Referer = 'moviekong.com';
-        var qpart = '|trailer';
+        var qpart = ' trailer';
 
         var search_more = function(query, onsuccessFn){
-            api.startNew().part('snippet').key(key);
-            var apiurl = api.q(query+qpart).maxResults(10).order('relevance')
+            query = encodeURIComponent(query+qpart);
+            searchapi.startNew().part('snippet').key(key);
+            var apiurl = searchapi.q(query).maxResults(10).order('relevance')
                 .type('video').videoDuration('short').build();
-            alert(apiurl);
             $http.get(apiurl).success(function(data,status){
-                console.log(data.items);
+                var id = data.items[0].id.videoId;
+                apiurl = watchapi.v(id).build();
+                //TODO: make use of the apiurl
             }).error(function(data,status){
                 alert(data.error.message);
-                alert(status);
+                console.log(data);
             });
         };
 		return {
